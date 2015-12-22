70447e2e803bfbc68a09d15f9b6bf1d99f771dbe
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2015-12-22 14:32:47 +0100

    Avoid var

--- plugin/src/main/scala/org/softwaremill/clippy/Advice.scala
+++ plugin/src/main/scala/org/softwaremill/clippy/Advice.scala
@@ -3,18 +3,15 @@ package org.softwaremill.clippy
 import java.net.URL
 
 import scala.xml._
+import scala.collection.JavaConversions._
 
 sealed trait Advice
 case class TypeMismatchAdvice(found: String, required: String, advice: String) extends Advice
 
 object Advices {
   def loadFromClasspath(): List[Advice] = {
-    val e = this.getClass.getClassLoader.getResources("clippy.xml")
-    var r: List[Advice] = Nil
-    while (e.hasMoreElements) {
-      r ++= loadFromURL(e.nextElement())
-    }
-    r
+    val i = enumerationAsScalaIterator(this.getClass.getClassLoader.getResources("clippy.xml"))
+    i.toList.flatMap(loadFromURL)
   }
 
   def loadFromURL(url: URL): List[Advice] = loadFromXml(XML.load(url))
