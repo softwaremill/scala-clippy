f8a51211a4bfcacda4cec6fcd6e37c9c4d3824bb
adamw <adam@warski.org>
2016-01-14 15:57:07 +0100

    Fixing tests

--- tests/src/test/scala/org/softwaremill/clippy/CompileTests.scala
+++ tests/src/test/scala/org/softwaremill/clippy/CompileTests.scala
@@ -1,10 +1,42 @@
 package org.softwaremill.clippy
 
-import org.scalatest.{Matchers, FlatSpec}
+import java.io.{FileOutputStream, File}
+import java.util.zip.GZIPOutputStream
 
+import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpec}
+
+import scala.io.Source
 import scala.tools.reflect.ToolBoxError
 
-class CompileTests extends FlatSpec with Matchers {
+class CompileTests extends FlatSpec with Matchers with BeforeAndAfterAll {
+
+  val localStoreDir = new File(System.getProperty("user.home"), ".clippy")
+  val localStore = new File(localStoreDir, "clippy.xml.gz")
+  val localStore2 = new File(localStoreDir, "clippy2.xml.gz")
+
+  /**
+   * Writing test xml data to where the plugin will expect to have it cached.
+   */
+  override protected def beforeAll() = {
+    super.beforeAll()
+    localStoreDir.mkdirs()
+    if (localStore.exists()) {
+      localStore.renameTo(localStore2)
+    }
+
+    val clippyXml = Source.fromInputStream(this.getClass.getResourceAsStream("/clippy.xml")).getLines().mkString("\n")
+    val os = new GZIPOutputStream(new FileOutputStream(localStore))
+    try os.write(clippyXml.getBytes("UTF-8")) finally os.close()
+  }
+
+  override protected def afterAll() = {
+    if (localStore2.exists()) {
+      localStore.delete()
+      localStore2.renameTo(localStore)
+    }
+
+    super.afterAll()
+  }
 
   val snippets = Map(
     "akka http" -> """
@@ -18,11 +50,11 @@ class CompileTests extends FlatSpec with Matchers {
                      |val r = complete("ok")
                      |
                      |Http().bindAndHandle(r, "localhost", 8080)
+                   """.stripMargin,
+    "macwire" -> """
+                     |class A()
+                     |val a = wire[A]
                    """.stripMargin
-  //    "macwire" -> """
-  //                   |class A()
-  //                   |val a = wire[A]
-  //                 """.stripMargin // TODO: old clippy.xml format
   )
 
   for ((name, s) <- snippets) {
