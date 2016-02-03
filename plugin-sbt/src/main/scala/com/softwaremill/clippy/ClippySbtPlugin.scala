4b7fc6f6f38851ff16478393d9073fbf2cdd973b
adamw <adam@warski.org>
2016-02-03 10:22:36 +0100

    Adding jar type in sbt plugin

--- plugin-sbt/src/main/scala/com/softwaremill/clippy/ClippySbtPlugin.scala
+++ plugin-sbt/src/main/scala/com/softwaremill/clippy/ClippySbtPlugin.scala
@@ -4,7 +4,7 @@ import sbt._
 
 object ClippySbtPlugin extends AutoPlugin {
   override def projectSettings = Seq(
-    addCompilerPlugin("com.softwaremill.clippy" %% "plugin" % ClippyBuildInfo.version)
+    addCompilerPlugin("com.softwaremill.clippy" %% "plugin" % ClippyBuildInfo.version % "bundle")
   )
 
   override def trigger = allRequirements
