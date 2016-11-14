a6a93f8cea94d0e49cce0cc980313c9ca8e4a365
Shane Delmore <shane@delmore.io>
2016-11-14 11:50:27 -0800

    Add TryWith[Closeable]

--- plugin/src/main/scala/com/softwaremill/clippy/Utils.scala
+++ plugin/src/main/scala/com/softwaremill/clippy/Utils.scala
@@ -1,6 +1,10 @@
 package com.softwaremill.clippy
 
 import java.io.{ByteArrayOutputStream, InputStream}
+import java.io.Closeable
+import scala.util.control.NonFatal
+import scala.util.{Failure, Try}
+
 
 object Utils {
   /**
@@ -40,5 +44,27 @@ object Utils {
       baos.toByteArray
     }
     finally is.close()
+  }
+
+  object TryWith {
+    def apply[C <: Closeable, R](resource: => C)(f: C => R): Try[R] =
+      Try(resource).flatMap(resourceInstance => {
+        try {
+          val returnValue = f(resourceInstance)
+          Try(resourceInstance.close()).map(_ => returnValue)
+        }
+        catch {
+          case NonFatal(exceptionInFunction) =>
+            try {
+              resourceInstance.close()
+              Failure(exceptionInFunction)
+            }
+            catch {
+              case NonFatal(exceptionInClose) =>
+                exceptionInFunction.addSuppressed(exceptionInClose)
+                Failure(exceptionInFunction)
+            }
+        }
+      })
   }
 }
