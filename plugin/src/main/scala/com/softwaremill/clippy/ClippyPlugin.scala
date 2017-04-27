99046c02020371cdaa9c091c4b0bc49789b790db
adamw <adam@warski.org>
2017-04-27 11:49:41 +0200

    Using URLClassLoader directly to solve 2.10 compilation error

--- plugin/src/main/scala/com/softwaremill/clippy/ClippyPlugin.scala
+++ plugin/src/main/scala/com/softwaremill/clippy/ClippyPlugin.scala
@@ -1,14 +1,13 @@
 package com.softwaremill.clippy
 
 import java.io.File
-import java.net.URL
+import java.net.{URL, URLClassLoader}
 import java.util.concurrent.TimeoutException
 
 import scala.collection.JavaConverters._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.reflect.internal.util.Position
-import scala.reflect.internal.util.ScalaClassLoader
 import scala.tools.nsc.Global
 import scala.tools.nsc.plugins.Plugin
 import scala.tools.nsc.plugins.PluginComponent
@@ -28,8 +27,8 @@ class ClippyPlugin(val global: Global) extends Plugin {
   var projectRoot: Option[File] = None
 
   lazy val localAdviceFiles = {
-    val classPathURLs = new PathResolver(global.settings).resultAsURLs
-    val classLoader = ScalaClassLoader.fromURLs(classPathURLs, getClass.getClassLoader)
+    val classPathURLs = new PathResolver(global.settings).result.asURLs
+    val classLoader = new URLClassLoader(classPathURLs.toArray, getClass.getClassLoader)
     classLoader.getResources("clippy.json").asScala.toList
   }
 
